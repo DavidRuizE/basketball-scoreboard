d57c247f8d359e6972ad6cd09cae118526065526
David <david02ruiz@gmail.com>
2022-12-21 22:23:08 -0500

    finished

---
+++ index.js
@@ -0,0 +1,30 @@
+let marcadorHost = document.getElementById("puntosHome")
+let marcadorGuest = document.getElementById("puntosGuest")
+contadorHost = 0
+contadorGuest = 0
+function aumentarH1(){
+    contadorHost+=1
+    marcadorHost.textContent= contadorHost
+}
+function aumentarH2(){
+    contadorHost+=2
+    marcadorHost.textContent= contadorHost
+}
+function aumentarH3(){
+    contadorHost+=3
+    marcadorHost.textContent= contadorHost
+}
+function aumentarG1(){
+    contadorGuest+=1
+    marcadorGuest.textContent= contadorGuest
+}
+function aumentarG2(){
+    contadorGuest+=2
+    marcadorGuest.textContent= contadorGuest
+}
+function aumentarG3(){
+    contadorGuest+=3
+    marcadorGuest.textContent= contadorGuest
+
+}
+
